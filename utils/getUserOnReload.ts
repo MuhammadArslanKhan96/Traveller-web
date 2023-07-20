99cfb69dec2929ba4728e22c16d8b41c149686d3
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2023-07-20 19:56:33 +0500

    fix: given any type to catch error for sometime

--- utils/getUserOnReload.ts
+++ utils/getUserOnReload.ts
@@ -15,12 +15,12 @@ export default async function getUserOnReload(
             `/api/users/get-user?role=${role}&email=${user.email}`
           );
           setUser(user2.data);
-        } catch (error) {
+        } catch (error: any) {
           console.log(error?.message);
         }
       }
     });
-  } catch (error) {
+  } catch (error: any) {
     console.log(error?.message);
   }
 }
