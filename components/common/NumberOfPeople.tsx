9ac12325b5acd4fa2b9f5aa6fcbb8ef056f03200
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2023-07-21 20:36:38 +0500

    fix: responsive edit booking page

--- components/common/NumberOfPeople.tsx
+++ components/common/NumberOfPeople.tsx
@@ -26,8 +26,8 @@ const NumberOfPeople = ({ travelers, selectValue }: NumberOfPeopleProps) => {
         setCurrentTraveler((pre: any) => ({ ...pre, [type]: pre[type] - 1 }))
     }
     return (
-        <div className="bg-white px-8 py-5 rounded-xl flex-col items-center gap-7 flex">
-            <div className="justify-center items-center gap-7 flex">
+        <div className="bg-white px-2 lg:px-8 py-5 rounded-xl flex-col items-center gap-7 flex">
+            <div className="justify-center items-center gap-7 flex max-lg:flex-col">
                 <div className=" gap-2.5 flex">
                     <div className="flex-col justify-center gap-2 flex">
                         <p className=" text-dark text-xs font-medium break-words">Adults</p>
